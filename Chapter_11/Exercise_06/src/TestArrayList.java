64afb2a5f3267c17c55a679a8cadfe96c016e07e
Hachem <o.p.s.hachem@gmail.com>
2026-07-27 23:57:35 +0100

    Submitted solution for Ex11-06 Part 2

--- Chapter_11/Exercise_06/src/TestArrayList.java
+++ Chapter_11/Exercise_06/src/TestArrayList.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class TestArrayList {
     public static void main(String[] args) {
-
+        ArrayList<Object> objects = new ArrayList<>();
+        objects.add(new Date());
+        objects.add("Hello World!");
+        objects.add(new Circle());
+        objects.add(new Loan());
+        for (Object obj: objects) {
+            System.out.println(obj);
+        }
     }
 }
